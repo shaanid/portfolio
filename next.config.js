9556e95fa9d172a463ed9faae3bdb657b57c8dea
shaanid <shanid@meridian.net.in>
2025-02-26 00:30:13 +0530

    feat: altered version

--- next.config.js
+++ next.config.js
@@ -2,11 +2,10 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  output: 'export',
-  // Optional: Add a trailing slash to all paths `/about` -> `/about/`
-  // trailingSlash: true,
-  // Optional: Change the output directory `out` -> `dist`
-  // distDir: 'dist',
+  output: 'export', // Required for static site export
+  images: { unoptimized: true }, // Required for GitHub Pages
+  // trailingSlash: true, // Optional: Adds trailing slashes to URLs
+  // distDir: 'out', // Optional: Change output directory (make sure to update GitHub Actions if changed)
 };
- 
+
 module.exports = nextConfig;
