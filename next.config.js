dd3deb3734d4b1e446c9b35a4e47f7afdeaa69f2
Shaanid <111045203+shaanid@users.noreply.github.com>
2025-03-15 05:25:44 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
-};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: 'export',
+//   images: {
+//     unoptimized: true,
+//   },
+// };
 
-module.exports = nextConfig;
+// module.exports = nextConfig;
+
+const isProd = process.env.NODE_ENV === "production";
+
+module.exports = {
+  output: "export", // Enables static export
+  images: { unoptimized: true }, // Fixes image optimization issues in static builds
+  basePath: isProd ? "/portfolio" : "",
+};
