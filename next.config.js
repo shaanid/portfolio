a2b581a491447edcb208e550b3edc12f3c275de4
shaanid <shanidmeridian.net.in>
2025-03-15 05:38:20 +0530

    fix: deployment

--- next.config.js
+++ next.config.js
@@ -1,17 +1,9 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   output: 'export',
-//   images: {
-//     unoptimized: true,
-//   },
-// };
-
-// module.exports = nextConfig;
-
-const isProd = process.env.NODE_ENV === "production";
-
-module.exports = {
-  output: "export", // Enables static export
-  images: { unoptimized: true }, // Fixes image optimization issues in static builds
-  basePath: isProd ? "/portfolio" : "",
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
 };
+
+module.exports = nextConfig;
